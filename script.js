d6cde2547246f0085a019c81c0addc6cbdd60b1e
Ruby Cardozo <rubyjunecar@gmail.com>
2024-01-24 16:07:55 -0500

    to

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#ruby').animate({
         marginTop: '5vh'
     }, 900)
     const subtitle = document.getElementById('subtitle')
-    setTimeout(function(){
+    setTimeout(function () {
         subtitle.style.opacity = '1';
     }, 1000)
     const line = document.getElementById('line')
-    setTimeout(function(){
+    setTimeout(function () {
         line.style.width = '50vw';
     }, 1100)
 
-    setTimeout(function(){
+    setTimeout(function () {
         $('#image').animate({
             marginRight: 0
         }, 900)
     }, 1300)
-    
 
-    projects = ['#one','#two','#three','#four','#five']
+
+    projects = ['#one', '#two', '#three', '#four', '#five', '#six']
     var loadTime = 900;
     var incProj = 400;
 
-    for(let i =0;i<projects.length;i++){
-        
-        setTimeout(function(){
+    for (let i = 0; i < projects.length; i++) {
+
+        setTimeout(function () {
             $(projects[i]).animate({
                 marginLeft: 0
-            }, 900 )
-        }, (loadTime + incProj*(i+1)))
+            }, 900)
+        }, (loadTime + incProj * (i + 1)))
     }
     const workhead = document.getElementById('workhead')
-    const line2 =document.getElementById('line2')
-    setTimeout(function(){
+    const line2 = document.getElementById('line2')
+    setTimeout(function () {
         workhead.style.opacity = '1';
         line2.style.width = '30vw';
 
-    }, loadTime + incProj*5)
+    }, loadTime + incProj * 5)
 
 });
 
@@ -49,42 +49,49 @@ $(document).ready(function () {
     let p = document.getElementById('p')
     $('#one').hover(
         function () {
-            $('#imgy').attr('src','images/noble.jpeg')
+            $('#imgy').attr('src', 'images/portfolio.png')
             p.innerHTML = 'Web Development • Web Design'
-            });
-  $('#two').hover(
+        });
+
+    $('#two').hover(
         function () {
-            $('#imgy').attr('src','images/Xtrafit.jpeg')
+            $('#imgy').attr('src', 'images/Xtrafit.jpeg')
             p.innerHTML = 'UX/UI Design • UX Research'
-            });
-   $('#three').hover(
+        });
+    $('#three').hover(
         function () {
-            $('#imgy').attr('src','images/portfolio.png')
-            p.innerHTML = 'Web Development • Web Design'
-            });  
+            $('#imgy').attr('src', 'images/weathernow.png')
+            p.innerHTML = 'Web Development • React.js'
+        });
+
     $('#four').hover(
         function () {
-            $('#imgy').attr('src','images/weathernow.png')
-            p.innerHTML = 'Illustration • Digital Art'
-            });   
+            $('#imgy').attr('src', 'images/todo.png')
+            p.innerHTML = 'Web Development • React.js'
+        });
     $('#five').hover(
         function () {
-            $('#imgy').attr('src','images/instagram.png')
+            $('#imgy').attr('src', 'images/noble.jpeg')
+            p.innerHTML = 'Web Development • Web Design'
+        });
+    $('#six').hover(
+        function () {
+            $('#imgy').attr('src', 'images/instagram.png')
             p.innerHTML = 'Illustration • Digital Art'
-            });                           
+        });
 });
 
 // JavaScript code to toggle mobile menu
 document.addEventListener('DOMContentLoaded', function () {
-  const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]');
-  const mobileMenu = document.getElementById('mobile-menu');
+    const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]');
+    const mobileMenu = document.getElementById('mobile-menu');
 
-  mobileMenuButton.addEventListener('click', function () {
-    const expanded = mobileMenuButton.getAttribute('aria-expanded') === 'true' || false;
-    mobileMenuButton.setAttribute('aria-expanded', !expanded);
-    mobileMenu.classList.toggle('hidden');
-  });
+    mobileMenuButton.addEventListener('click', function () {
+        const expanded = mobileMenuButton.getAttribute('aria-expanded') === 'true' || false;
+        mobileMenuButton.setAttribute('aria-expanded', !expanded);
+        mobileMenu.classList.toggle('hidden');
+    });
 });
 
-    
+
 
